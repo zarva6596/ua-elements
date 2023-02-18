27dda295841087fbc363ef0c0eb3e48a6f194c74
zarva6596 <zarva6596@gmail.com>
2023-02-18 10:44:59 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </header>
+
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
---
+++ src/views/home/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <el-drawer model-value="true" />
+  </div>
+</template>
+<script setup lang="ts">
+import ElDrawer from '@/components/drawer/ElDrawer.vue';
+</script>
